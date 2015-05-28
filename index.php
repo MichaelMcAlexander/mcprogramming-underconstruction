9e0db31b5c0645e6c2bc0bb6561dd86afb7b0fa9
Mike McAlexander <mcalexandems@vcu.edu>
2015-05-28 08:56:44 -0400

    trying to add to github

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>McProgramming</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet'>
+		<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+		<link rel="shortcut icon" href="http://www.mcprogramming.org/favicon.ico" type="image/x-icon" />
+	</head>
+	<body>
+		hello
+	</body>
+</html>
